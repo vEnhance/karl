fdb2361f8964c747521b6f9c1c406c7a69c758d6
Evan Chen <evanchen@mit.edu>
2016-02-03 23:41:16 -0500

    Cosmetic changes to ID display

--- view_set.php
+++ view_set.php
@@ -101,7 +101,7 @@ echo "</h2>";
 
 		echo "<td valign=\"top\">" . "\n";
 		echo "<a href=\"view_problem.php?id=$id\" class=\"probnum\">{$prob_row['zindex']}.</a> "; 
-		echo "<div class=\"set_problem_id\">$id</div>";
+		echo "<div class=\"set_problem_id\">[$id]</div>";
 
 		// Print votes and stuff
 		if ($this_votes > 0) {
